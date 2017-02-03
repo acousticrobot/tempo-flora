9c802d232be675596bcb5c908142c1cadb24d14c
Jonathan Gabel <hello@jonathangabel.com>
2017-02-03 11:04:53 -0600

    add game demo

---
+++ client/app/bundles/TicTacToe/startup/TicTacToeApp.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TicTacToeContainer from '../containers/TicTacToeContainer';
+
+const TicTacToeApp = (props, _railsContext) => (
+  <TicTacToeContainer />
+);
+
+export default TicTacToeApp;
